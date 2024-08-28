bfb6ebe5f6c42988c32f5b04e95541cc34ce4aa0
Adam <adam.wajszczak.5@gmail.com>
2024-08-28 14:46:19 +0200

    Git init

--- app/page.js
+++ app/page.js
@@ -1,113 +1,93 @@
 import Image from "next/image";
+import Link from "next/link";
+import { FaExternalLinkAlt } from "react-icons/fa";
+
+import codeImg1 from "@/public/Code_1.png";
+import codeImg2 from "@/public/Code_2.png";
+
+import courseImg from "@/public/courseimg.jpg";
+
+import electronicImg from "@/public/electronic/22.jpg";
 
 export default function Home() {
+  const imageSizes = {
+    width: 700,
+    height: 500,
+  };
+  const linkedImageSizes = {
+    width: 200,
+    height: 100,
+  };
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
+    <main className="flex-wrap flex-shrink whitespace-nowrap flex flex-col items-center justify-center">
+      <section className="main-text flex items-center justify-center flex-col">
+        <h2 className="text-3xl md:text-7xl mt-10">Strona</h2>
+        <p>
+          Została stworzona w celach dokumentacji rezultatu opisanego w IPRZU
         </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+        <p>Strona powstała w oparciu o Framework biblioteki React - Next.JS.</p>
+        <p>Poniżej znajdują się zdjęcia kodu</p>
+        <figure className="main-figure">
+          <Image
+            src={codeImg1}
+            className="main-figure-image"
+            alt="Zdjęcie kodu numer 1"
+            width={imageSizes.width}
+            height={imageSizes.height}
+          />
+          <figcaption>Zdjęcie kodu nr. 1</figcaption>
+        </figure>
+        <figure className="main-figure">
+          <Image
+            className="main-figure-image"
+            src={codeImg2}
+            alt="Zdjęcie kodu numer 2"
+            width={imageSizes.width}
+            height={imageSizes.height}
+          />
+          <figcaption>Zdjęcie kodu nr. 2</figcaption>
+        </figure>
+      </section>
+      <section className="main-text flex items-center justify-center flex-col mt-20">
+        <h2 className="text-3xl md:text-7xl mt-10">Kurs z j.Angielskiego</h2>
+        <p>Na stronie znajduje się certyfikat ukończenia kursu</p>
+        <Link
+          className=" text-blue-500 font-extrabold p-2 shadow-lg shadow-gray"
+          href="/kurs-angielski"
+        >
+          <div className="flex items-end flex-col">
+            <FaExternalLinkAlt className="absolute z-10 m-2" />
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src={courseImg}
+              className="blur-sm hover:blur-none transition-all"
+              width={linkedImageSizes.width}
+              height={linkedImageSizes.height}
+              alt="Odnośnik do strony z certyfikatem"
             />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+            <p>Odsyłacz</p>
+          </div>
+        </Link>
+      </section>
+      <section className="main-text flex items-center justify-center flex-col mt-20">
+        <h2 className="text-3xl md:text-7xl mt-10">Układy elektroniczne</h2>
+        <p>Na stronie znajdują się również zdjęcia układów elektronicznych</p>
+        <Link
+          className=" text-blue-500 font-extrabold p-2 shadow-lg shadow-gray"
+          href="/elektronika"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+          <div className="flex items-end flex-col">
+            <FaExternalLinkAlt className="absolute z-10 m-2" />
+            <Image
+              src={electronicImg}
+              className="blur-sm hover:blur-none transition-all"
+              width={linkedImageSizes.width}
+              height={linkedImageSizes.height}
+              alt="Odnośnik do zdjęć układów elektronicznych"
+            />
+            <p>Odsyłacz</p>
+          </div>
+        </Link>
+      </section>
     </main>
   );
 }
